ab992046434fd23dc41f2d9100488a1afcddb07d
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2023-06-18 11:58:25 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { LiFiWidget } from '@lifi/widget';
 
 export const widgetConfig: Partial<WidgetConfig> = {
   containerStyle: {
-    border: `1px solid rgb(234, 234, 234)`,
     borderRadius: '16px',
   },
 };

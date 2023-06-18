8a49ec8898bd741ce232f4561425b495693bdcff
THANATHORN <99862291+THANABusiness@users.noreply.github.com>
2023-06-18 12:06:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { LiFiWidget } from '@lifi/widget';
 
 export const widgetConfig: Partial<WidgetConfig> = {
   containerStyle: {
+    width: '100%',
     borderRadius: '16px',
   },
 };
 
 export function App() {
-  return <LiFiWidget config={widgetConfig} integrator="vite-example" />;
+  return <LiFiWidget config={widgetConfig} integrator="Thanaplus" />;
 }
